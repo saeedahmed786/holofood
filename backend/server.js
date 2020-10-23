ba454cdf170f3bad2c29d5714cb634128bade8f3
Saeed Ahmed <saeedchachar987654@gmail.com>
2020-10-23 14:18:33 -0700

    new change

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,8 @@ app.use('/api/products', productRoutes);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('../frontend/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'app', '../frontend', 'build', 'index.html'));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'app', 'frontend', 'build', 'index.html'));
 
     });
 }
