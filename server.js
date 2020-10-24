932b7b30b4e4c3a64255d6a13a6903cabcf3079f
Saeed Ahmed <saeedchachar987654@gmail.com>
2020-10-24 02:10:55 -0700

    new

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ mongoose.connect(config.mongoURI, {
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(__dirname+ '/uploads'));
+app.use(express.static(__dirname+ '/Routes/uploads'));
 app.use(express.urlencoded({extended: true}));
-app.use('/uploads', express.static(__dirname+ '/uploads'));
+app.use('/Routes/uploads', express.static(__dirname+ '/Routes/uploads'));
 app.use(cookieParser());
 app.use('/api/users', UserRoutes);
 app.use('/api/products', productRoutes);
