86e128718f951667156901086b66e3188f2394ea
Saeed Ahmed <saeedchachar987654@gmail.com>
2020-12-02 18:32:16 -0800

    first

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,6 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     jwtSecret: process.env.JWT_SECRET,
     jwtExpire: process.env.JWT_EXPIRE ,
-    api_key: process.env.epi_key,
+    api_key: process.env.api_key,
     DOMAIN: process.env.DOMAIN
 };
